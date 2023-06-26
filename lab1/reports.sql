f4386d6a4637c07ffa855684d2dbd8bb550b45a5
Sujal Kokh Shrestha <sujal.kokh@gmail.com>
2023-06-26 22:54:26 +0545

    la1: part 3, reporting and trigger

---
+++ lab1/reports.sql
@@ -0,0 +1,17 @@
+-- 1. Find the list of Users that are active.
+
+-- 2. Retrive the parents_id, user_id, name, email, mobile_number of all the parents
+
+-- 3. Retrive the student_id, user_id, name, email, mobile_number of all the students
+
+-- 4. Retrive the teacher_id, user_id, name, email, mobile_number of all the teachers
+
+-- 5. Find the list of parents with total number childrens 
+
+-- 6. Find the list of teachers who teach Computer subject(Any subject)
+
+-- 7. Write a SQL that shows the total merit points of all the students for subject Computer of Class 10.
+
+-- 8. Write a SQL that shows the merit points for all subjects of any student.
+
+-- 9. To simulate a notification event, lets create a notifications table that contains following columns (id, user_device_token, headers, message, deeplink, is_notification_sent, created_at, deleted_at). Create a trigger that inserts data into the notifications table every time a data is added in merit_points table. Use "Merit point update" as header and "Merit point has been updated" as body, "https://deeplink.testdeeplink.com/deeplink" as deeplink, false as is_notificat ion_sent.
